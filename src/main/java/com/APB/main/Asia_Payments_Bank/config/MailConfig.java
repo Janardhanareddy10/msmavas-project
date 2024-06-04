cff0cf6ae8d1a6ba70c43b86d655e8936f43f72a
Janardhanareddy143 <157009810+Janardhanareddy143@users.noreply.github.com>
2024-06-04 15:09:14 +0530

    Second commit

--- src/main/java/com/APB/main/Asia_Payments_Bank/config/MailConfig.java
+++ src/main/java/com/APB/main/Asia_Payments_Bank/config/MailConfig.java
@@ -6,7 +6,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
-//config
+
 @Configuration
 public class MailConfig {
 
